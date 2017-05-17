10bdc6df2e7c8c0fb07ef6d805e4e5f438abf198
Teapoth <timotheedavid@orange.fr>
2017-05-17 22:07:00 +0200

    Tests

--- src/tests/JUnitLayer.java
+++ src/tests/JUnitLayer.java
@@ -14,10 +14,10 @@ public class JUnitLayer {
 	
 	@Before
 	public void setUp() {
-		double[] val = {2,6,4};
-		double[][] weight = {{1,2,3},
-							 {2,3,0},
-							 {5,6,4}};
+		double[] val = {-0.5,0.5,0.8};
+		double[][] weight = {{1,0.5,0.2},
+							 {0.3,0.3,0},
+							 {-0.7,0.6,-0.4}};
 		underTestLayer = new Layer(val, weight);
 		underTestLayer2 = new Layer(val, weight, underTestLayer);
 		
@@ -31,20 +31,22 @@ public class JUnitLayer {
 	@Test
 	public void testActivate() {
 		underTestLayer.activate();
-		double[] calculatedResult = {1.65417,
-									1.715879,
-									1.714749};
+		double[] calculatedResult = {-0.79294683,
+									0.79294683,
+									1.13942064};
 		Assert.assertArrayEquals(underTestLayer.getValues(), calculatedResult, 0.00001);	
 	}
 	
 	@Test
 	public void testPropagate() {
-		underTestLayer2.propagate();
-		double[] calculatedResult = {2,6,4};
 		for (int i=0; i<3; i++){
 			System.out.println(underTestLayer.getValues()[i]);
 		}
-		System.out.println(1.7159*Math.tanh(5));
+		underTestLayer2.propagate();
+		double[] calculatedResult = {-0.154015,0,0.546549};
+		for (int i=0; i<3; i++){
+			System.out.println(i+": " + underTestLayer.getValues()[i]);
+		}
 		Assert.assertArrayEquals(underTestLayer.getValues(), calculatedResult, 0.00001);
 	}
 	
